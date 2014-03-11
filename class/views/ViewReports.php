858701e86a1f0f94c9066f69a108d7dbcc155375
Jeremy Booker <jeremy.booker@gmail.com>
2014-03-11 13:21:29 -0400

    Update report form to fix datepicker class name.

--- class/views/ViewReports.php
+++ class/views/ViewReports.php
@@ -19,11 +19,11 @@ class ViewReports extends SLCView {
         //$form->addText('start_date', date('m/d/Y', strtotime('-1 month')));
         $form->addText('start_date', '');   // date pickers are initially blank
         $form->setSize('start_date', 10);
-        $form->setExtra('start_date', 'class="datepicker"');
+        $form->addCssClass('start_date', 'datepicker');
         //$form->addText('end_date', $current_date);
         $form->addText('end_date', '');     // date pickers are initially blank
         $form->setSize('end_date', 10);
-        $form->setExtra('end_date', 'class="datepicker"');
+        $form->addCssClass('end_date', 'datepicker');
         $content['START_DATE'] = $form->get('start_date');
         $content['END_DATE'] = $form->get('end_date');
 
