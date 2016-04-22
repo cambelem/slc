93bda2230e75a9ff8308ab2ba7c4582ed303f76b
Ted Eberhard <eberhardtm@users.noreply.github.com>
2016-04-22 13:22:40 -0400

    Update head.js

--- javascript/report/head.js
+++ javascript/report/head.js
@@ -2,4 +2,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.8/react-with-addons.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.8/react-dom.js"></script>
 <script src="https://fb.me/JSXTransformer-0.13.1.js"></script>
-<script type = "text/jsx" src = "mod/slc/javascript/report/ReportPicker.jsx"></script>
+<script type = "text/jsx" src = "{source_http}mod/slc/javascript/report/ReportPicker.jsx"></script>
