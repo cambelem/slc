7f4b783dbc9604901610a95c680a844179ac02b5
Ted Eberhard <eberhardtm@users.noreply.github.com>
2016-04-22 12:06:36 -0400

    update jsx src

--- javascript/viewClient/head.js
+++ javascript/viewClient/head.js
@@ -3,8 +3,8 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/react/0.14.8/react-dom.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/react-bootstrap/0.27.3/react-bootstrap.js"></script>
 <script src="https://fb.me/JSXTransformer-0.13.1.js"></script>
-<script type="text/jsx" src="mod/slc/javascript/viewClient/ModalForm.jsx"></script>
-<script type="text/jsx" src="mod/slc/javascript/viewClient/ViewClientMain.jsx"></script>
+<script type="text/jsx" src="{source_http}mod/slc/javascript/viewClient/ModalForm.jsx"></script>
+<script type="text/jsx" src="{source_http}mod/slc/javascript/viewClient/ViewClientMain.jsx"></script>
 <script type="text/javascript">
 var banner_id = {BANNER_ID};
 </script>
