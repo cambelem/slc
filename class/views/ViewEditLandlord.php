edb964fca2cf53f2d91e6568651fc7d12d1394af
Ted Eberhard <eberhardtm@users.noreply.github.com>
2016-08-19 13:51:26 -0400

    Update ViewEditLandlord.php

--- class/views/ViewEditLandlord.php
+++ class/views/ViewEditLandlord.php
@@ -9,7 +9,7 @@ class ViewEditLandlord extends View {
 
 		// \javascriptMod('slc', 'editLandlord');
 		// javascript('jquery');
-
+		$content['source_http'] = PHPWS_SOURCE_HTTP;
 	 	$HTMLcontent .= \PHPWS_Template::process($content, 'slc', 'EditLandlords.tpl');
 	 	
 	 	return parent::useTemplate($HTMLcontent); // Insert into the accessible div
